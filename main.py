2ccac070a3af2afaea755d076524c48e764a1bb5
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-13 16:17:40 +0800

    Buy the all the products you can every 5 seconds

--- main.py
+++ main.py
@@ -16,6 +16,28 @@ last_action_time = start_time
 while time.time() < end_time:
     current_time = time.time()
 
+    # Check What To Buy
+    if current_time - last_action_time >= 5:
+        money = int(driver.find_element(By.ID, 'money').text)
+        store = driver.find_element(By.ID, 'store')
+        products = store.find_elements(By.TAG_NAME, 'div')
+
+        # Using a reverse loop to start checking from the most expensive product
+        for product in products[::-1]:
+            try:
+                click_this = product.find_element(By.TAG_NAME, 'b')
+                elements = click_this.text
+                amount = int(elements.split(' - ')[1].replace(',', ''))
+                if money > amount:
+                    product.click()
+                    money -= amount
+            except NoSuchElementException:
+                print(f"Didn't find the element")
+                continue
+            except IndexError:
+                continue
+        last_action_time = current_time
+
     # Clicking the Cooking
     cookie_button = driver.find_element(By.ID, 'cookie')
     cookie_button.click()
